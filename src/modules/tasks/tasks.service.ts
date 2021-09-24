fde285d1974bf6942978e85a958828df843ebe17
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-24 12:40:25 +0530

    feat: Create service functions for getAllTask and CreateTask

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Task } from './models/interfaces/task.model';
+import { TaskStatus } from 'src/enums/TaskStatus.enum';
+import { CreateTaskDto } from './models/dto/create-tasl.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    private tasks : Task[]= [];
+
+    getAllTasks() : Task[] {
+        return this.tasks;
+    }
+
+    createTask(createTaskDto: CreateTaskDto) :Task {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.DONE
+        }
+        this.tasks.push(task);
+        return task;
+    }
+}
