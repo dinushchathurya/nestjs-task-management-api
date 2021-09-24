dfc884ab4c6d88280d10f55125d96c7f16e45bf9
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-24 17:45:47 +0530

    refactor: Add validation to GetTaskFilter DTO

--- src/modules/tasks/models/dto/get-tasks-filter.dto.ts
+++ src/modules/tasks/models/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,14 @@
 import { TaskStatus } from "src/enums/TaskStatus.enum";
+import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
 
 export class GetTasksFilterDto {
+
+    @IsOptional()
+    @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     status: TaskStatus
+
+    @IsOptional()
+    @IsNotEmpty()
     search :string
+    
 }
