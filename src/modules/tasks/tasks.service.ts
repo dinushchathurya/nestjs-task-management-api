2734cac54d19a99f1cc9dd2a55725f7b369d911f
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-24 16:55:54 +0530

    refactor: Configure task not found error in deleteTask

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -62,7 +62,8 @@ export class TasksService {
     }
 
     deleteTask(id: string) : void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const task = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== task.id);
     }
 
 }
