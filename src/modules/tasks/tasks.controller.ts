5a3030e5a0004d2232075b61f51124290cf0d680
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-25 20:26:16 +0530

    feat: Create task update controller

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe, Body, Delete } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe, Body, Delete, Patch } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 import { Task } from 'src/models/tasks/entities/task.entity';
 import { CreateTaskDto } from 'src/models/tasks/dto/create-task.dto';
+import { TaskStatusValidationPipe } from 'src/common/validations/task-status-validatiom.pipe';
+import { TaskStatus } from 'src/common/enums/TaskStatus.enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -18,6 +20,11 @@ export class TasksController {
     @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto:CreateTaskDto): Promise<Task> {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus) : Promise<Task>{
+        return this.tasksService.updateTaskStatus(id, status);
     }
 
     @Delete('/:id')
