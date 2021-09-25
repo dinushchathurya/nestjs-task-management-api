e05d5822073807e88f667e18c5a58e687b1aa9f8
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-25 09:19:33 +0530

    feat: Create repositiory method for create new task

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -1,7 +1,23 @@
 import { Repository, EntityRepository } from "typeorm";
+
 import { Task } from "src/models/tasks/entities/task.entity";
+import { CreateTaskDto } from "src/models/tasks/dto/create-task.dto";
+import { TaskStatus } from "src/common/enums/TaskStatus.enum";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
     
+    async createTask(createTaskDto:CreateTaskDto): Promise<Task> {
+
+        const { title, description } = createTaskDto;
+
+        const task = new Task();
+        task.title = title;
+        task.description = description;
+        task.status= TaskStatus.OPEN;
+        await task.save();
+
+        return task;
+
+    }
 }
