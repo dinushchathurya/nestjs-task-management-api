6c01f7d4cc944ca7341914cada5d62939f007c17
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-25 21:33:16 +0530

    config: Peering User module in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './../ormconfig';
 
 import { TasksModule } from './modules/tasks/tasks.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(config),
-        TasksModule
+        TasksModule,
+        AuthModule
     ],
     controllers: [],
     providers: [],
