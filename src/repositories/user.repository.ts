f0bcf980a9c0597a935a11b5354ea33f22325764
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-26 09:25:27 +0530

    refactor: Hashing plain password using bcrypt

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,10 @@
 import { EntityRepository, Repository } from "typeorm";
+import { InternalServerErrorException, ConflictException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 
 import { User } from "src/models/users/entities/user.entity";
 import { AuthCredentialsDto } from "src/models/users/dto/auth-credentials.dto";
-import { InternalServerErrorException, ConflictException } from "@nestjs/common";
+
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -13,7 +15,8 @@ export class UserRepository extends Repository<User> {
 
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashedPassword(password, user.salt);
 
         try {
             await user.save();
@@ -24,5 +27,9 @@ export class UserRepository extends Repository<User> {
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashedPassword(password: string, salt: string): Promise<string> {
+        return  bcrypt.hash(password, salt);
     }
 }
